6ad832b5dfd4d1479f2de345eafbe10aa5b34762
aozkan9 <bledwarrior@gmail.com>
2021-04-19 14:34:37 +0300

    Finished

--- app/src/main/java/com/example/navigations/SecondFragment.java
+++ app/src/main/java/com/example/navigations/SecondFragment.java
@@ -2,11 +2,17 @@ package com.example.navigations;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class SecondFragment extends Fragment {
@@ -20,7 +26,6 @@ public class SecondFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,7 +33,25 @@ public class SecondFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
+    //gorunumler olusturulduktan sonra calistiralak metodlari burada tanimlariz
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Button button = view.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFirst(v);
+            }
+        });
+    }
+
     public void goToFirst(View view){
+
+        NavDirections action = SecondFragmentDirections.actionSecondFragmentToFirstFragment();
+        Navigation.findNavController(view).navigate(action);
 
     }
 }
